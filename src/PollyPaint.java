35f7dc30e1c7f488415736c36c2f947b8469aecb
Isaac Bake <isaac@blake.earth>
2020-02-08 14:25:23 -0500

    Fix Java -> Processing translation issues

--- src/PollyPaint.java
+++ src/PollyPaint.java
@@ -1,10 +1,15 @@
+package src;
 import processing.core.PApplet;
 
 public class PollyPaint extends PApplet {
 
 	public static void main(String[] passedArgs) {
-		String[] appletArgs = new String[] { "PollyPaint" };
-		PApplet.main(appletArgs);
+		String[] appletArgs = { "PollyPaint" };
+		if (passedArgs != null) {
+			PApplet.main(concat(appletArgs, passedArgs));
+		} else {
+			PApplet.main(appletArgs);
+		} 
     }
 		
 	public void settings() {
