257dd36e9eef5ee4f9d1087184229b0a78160153
Isaac Bake <isaac@blake.earth>
2020-02-12 15:38:27 -0500

    Add withinScope

--- src/backend/PollyObject.java
+++ src/backend/PollyObject.java
@@ -2,6 +2,7 @@ package backend;
 
 import processing.core.PApplet;
 import processing.core.PConstants;
+import processing.core.PVector;
 
 public abstract class PollyObject {
     protected PApplet sketch;
@@ -32,9 +33,70 @@ public abstract class PollyObject {
         return new float[]{-1, -1};
     }
 
+/* description: checks if the provided x, y are within the pivoted bounding box of this
+    shape */
     protected boolean withinScope(float x, float y) {
-        if (x < boundingBox[0] && x > -boundingBox[0] && y < boundingBox[1] && y > -boundingBox[1]) return true;
+        // approach: rotate the given x, y by the NEGATIVE current rotation
+        // this will move it back into the original bounding box (based on width and height)
+        // if it is within the shape
+
+        float width = boundingBox[0];
+        float height = boundingBox[1];
+
+
+        PVector rotatedPoint = rotateAbout(new PVector(x, y), new PVector(xpos, ypos), -rot);
+
+        if ((rotatedPoint.x >= xpos - width && rotatedPoint.x <= xpos + width) && 
+            (rotatedPoint.y >= ypos - height && rotatedPoint.y <= ypos + height)) {
+                return true;
+        }
         return false;
+    }
+
+    protected PVector[] getBoundingBoxPoints(float width, float height) {
+        // this uses the existing bounding box system as much as possible
+        // using width and height to generate four points
+        // these points are NOT rotated per this PollyObject's rotation
+
+        // represented with four PVectors: topLeft, topRight, bottomRight, bottomLeft (like NESW)
+        PVector[] boundingBoxPoints = new PVector[4];
+        // topLeft
+        boundingBoxPoints[0] = new PVector(-width, -height);
+        // topRight
+        boundingBoxPoints[1] = new PVector(width, -height);
+        // bottomRight
+        boundingBoxPoints[2] = new PVector(width, height);
+        // bottomLeft
+        boundingBoxPoints[3] = new PVector(-width, height);
+        return boundingBoxPoints;
+    }
+
+    protected PVector[] getRotatedBoundingBoxPoints(float width, float height, float rot) {
+        PVector[] rotatedBoundingBoxPoints = new PVector[4];
+        PVector[] boundingBoxPoints = getBoundingBoxPoints(width, height);
+        PVector center = new PVector(xpos, ypos);
+        for (int i = 0; i < 4; i++) {
+            rotatedBoundingBoxPoints[i] = rotateAbout(boundingBoxPoints[i], center, rot);
+        }
+        return rotatedBoundingBoxPoints;
+    }
+
+    protected PVector rotateAbout(PVector rotatePoint, PVector aboutPoint, float degrees) {
+        PVector finalPoint = new PVector(0, 0);
+
+        // make finalPoint like rotatePoint as if aboutPoint were the origin
+        finalPoint.x = rotatePoint.x - aboutPoint.x;
+        finalPoint.y = rotatePoint.y - aboutPoint.y;
+
+        // rotate finalPoint about the origin
+        finalPoint.x = finalPoint.x * (float) Math.cos(degrees) -  finalPoint.y * (float) Math.sin(degrees);
+        finalPoint.y = finalPoint.x * (float) Math.sin(degrees) +  finalPoint.y * (float) Math.cos(degrees);
+
+        // translate finalPoint back to where it should be based on aboutPoint
+        finalPoint.x += aboutPoint.x;
+        finalPoint.y += aboutPoint.y;
+
+        return finalPoint;
     }
 
     protected void display() {    }
