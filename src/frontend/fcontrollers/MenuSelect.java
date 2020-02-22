158c5b3d4398fc87a62bd201ad5842401bf2e4ad
Hunter Chasens <hunter.chasens18@ncf.edu>
2020-02-22 13:50:18 -0500

    added MenuSelect class

---
+++ src/frontend/fcontrollers/MenuSelect.java
@@ -0,0 +1,28 @@
+package frontend.fcontrollers;
+/**
+ * <h1>MenuSelect</h1>
+ * MenuSelect is the class responsable for displaying the top buttonbar responsable for chaning topChoice in the GUI menu.
+ * MenuSelect is a buttonbar whith diffrent menu options, selecting an option will display a diffrenttoolbar menu below it.
+ *
+ *
+ * @author Hunter Chasens
+ * @version 1.0
+ * @since 02.12.2019
+ *
+ */
+
+import frontend.*;
+import frontend.controlP5.*;
+
+public class MenuSelect extends FButtonBar{
+  public MenuSelect(ControlP5 cp5, FToolbar parrent, GUI gui){
+    super(cp5, "MenuSelect", parrent, gui);
+    super.bb.plugTo(this);
+  }
+
+
+  public void MenuSelect(int val){
+
+  }
+
+}
