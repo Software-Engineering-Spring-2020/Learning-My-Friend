c4c6517ae4045098665a7198836d2f95e004e728
Isaac Bake <isaac@blake.earth>
2020-02-22 01:16:31 -0500

    Implement better (still incomplete) InteractiveTextBoxes

--- src/backend/objects/InteractiveTextBox.java
+++ src/backend/objects/InteractiveTextBox.java
@@ -7,6 +7,7 @@ import backend.TextObject;
 import processing.core.PApplet;
 import processing.core.PFont;
 import processing.core.PGraphics;
+import processing.core.PVector;
 import processing.core.PConstants;
 
 public class InteractiveTextBox extends TextObject implements Serializable {
@@ -19,41 +20,79 @@ public class InteractiveTextBox extends TextObject implements Serializable {
     }
     private String str, font;
     private float textSize;
-    private int charactersPerLine;
+    private ArrayList<Float> lineLengths;
     transient int cursorIndex;
     transient private PFont pFont;
+    private int charactersSinceNewLine;
+    private int charactersPerLine;
+    private final int LINE_SPACING = 16;
 
-  public InteractiveTextBox(PApplet sketch, float x, float y, float x2, float strokeWeight, int[] fillColor, int[] boarderColor, String font, float textSize){
+  public InteractiveTextBox(PApplet sketch, float x, float y, float width, float strokeWeight, int[] fillColor, int[] boarderColor, String font, float textSize){
     super(sketch, x, y, strokeWeight, fillColor, boarderColor);
     this.font = font;
     this.pFont = sketch.createFont(font, 1);
     this.textSize = textSize;
-    this.str = "NEW TEXT BOX";
+    this.lineLengths = new ArrayList<Float>();
+    this.lineLengths.add(0f);
+    this.str = "";
+    this.charactersPerLine = (int) (width / sketch.textWidth('o'));
+    String example = "NEW TEXT BOX";
+    char[] exampleCharacters = example.toCharArray();
+    for (char c: exampleCharacters) {
+        addCharacter(c);
+    }
     sketch.textSize(textSize);
-    pixelWidth = x2;
-    charactersPerLine = (int) (x2 / sketch.textWidth('o'));
-    pixelHeight = (str.length() / charactersPerLine) * textSize;
+    pixelWidth = width;
+    pixelHeight = lineLengths.size() * textSize;
     cursorIndex = str.length();
+    System.out.println(textSize);
+    System.out.println(pixelWidth);
+    System.out.println(pixelHeight);
     fillColor[3] = 255;
   }
 
+  /**
+   * Displays the changed bounding box and the text cursor.
+   */
   public void showBoundingBox() {
       super.showBoundingBox();
       sketch.push();
       sketch.stroke(0, 0, 0);
       sketch.strokeWeight(2);
-      float[] cursorPos = {xpos, ypos};
-      char[] characters = str.toCharArray();
+      float[] cursorPos = {xcenter - pixelWidth / 2, ycenter - pixelHeight / 2 + textSize / 2};
+      String untilCursor = str.substring(0, cursorIndex);
+      char[] characters = untilCursor.toCharArray();
       for (int i = 0; i < characters.length; i++) {
         cursorPos[0] += sketch.textWidth(characters[i]);
-        if (i % charactersPerLine == 0) {
-            cursorPos[0] = xpos;
-            cursorPos[1] = textSize;
+        if (cursorPos[0] >= xcenter - pixelWidth / 2 + pixelWidth) {
+            cursorPos[0] = xcenter - pixelWidth / 2;
+            cursorPos[1] += textSize;
         }
       }
       sketch.line(cursorPos[0], cursorPos[1] + textSize / 2, cursorPos[0], cursorPos[1] - textSize / 2);
       sketch.pop();
   }
+
+  protected PVector[] getBoundingBoxPoints(float xcenter, float ycenter) {
+    float width = pixelWidth*zoom;
+    float height = pixelHeight*zoom;
+    offset = 3;
+    // this uses the existing bounding box system as much as possible
+    // using width and height to generate four points
+    // these points are NOT rotated per this PollyObject's rotation
+
+    // represented with four PVectors: topLeft, topRight, bottomRight, bottomLeft (like NESW)
+    PVector[] boundingBoxPoints = new PVector[4];
+    // topLeft
+    boundingBoxPoints[0] = new PVector(-width/2 + xcenter - offset, -height/2 + ycenter - offset);
+    // topRight
+    boundingBoxPoints[1] = new PVector(width/2 + xcenter + offset, -height/2 + ycenter - offset);
+    // bottomRight
+    boundingBoxPoints[2] = new PVector(width/2 + xcenter + offset, height/2 + ycenter + offset);
+    // bottomLeft
+    boundingBoxPoints[3] = new PVector(-width/2 + xcenter - offset, height/2 + ycenter + offset);
+    return boundingBoxPoints;
+}
 
   /**
    * Adjusts the width and height of the InteractiveTextBox using
@@ -61,10 +100,31 @@ public class InteractiveTextBox extends TextObject implements Serializable {
    * 
    * @param x2 The new (abolute) width.
    */
-  protected void changeWidth(float x2) {
-    pixelWidth = x2;
-    charactersPerLine = (int) (x2 / sketch.textWidth('o'));
-    pixelHeight = (str.length() / charactersPerLine) * textSize;
+  protected void changeWidth(float width) {
+    pixelWidth = width;
+    char[] temp = str.toCharArray();
+    str = "";
+    pixelHeight = 0;
+    for (int i = 0; i < temp.length; i++) {
+        addCharacter(temp[i]);
+    }
+    pixelHeight = lineLengths.size() * textSize;
+  }
+
+  private void addLine() {
+    ycenter += (textSize + LINE_SPACING) / 2f;
+    pixelHeight += textSize + LINE_SPACING;
+    lineLengths.add(0f);
+  }
+
+  private void removeLine() {
+    ycenter -= (textSize + LINE_SPACING) / 2f;
+    pixelHeight -= textSize + LINE_SPACING;
+    lineLengths.add(0f);
+  }
+
+  private int getLineNumber() {
+      return 0;
   }
 
   /**
@@ -121,9 +181,14 @@ public class InteractiveTextBox extends TextObject implements Serializable {
             j++;
         }
     }
+    charactersSinceNewLine++;
+    if (c == PConstants.ENTER || c == PConstants.RETURN) {
+        moveCursor(Direction.DOWN);
+        addLine();
+    }
+    if (charactersSinceNewLine % charactersPerLine == 0) addLine();
     cursorIndex++;
     str = new String(newCharacters);
-    pixelHeight = (str.length() / charactersPerLine) * textSize;
   }
 
   /**
@@ -131,17 +196,21 @@ public class InteractiveTextBox extends TextObject implements Serializable {
    */
   protected void removeCharacter() {
     char[] characters = str.toCharArray();
-    char[] newCharacters = new char[characters.length - 1];
-    int j = 0;
-    for (int i = 0; i < characters.length + 1; i++) {
-        if (i != cursorIndex) {
-            newCharacters[i] = characters[j];
+    if (str.length() > 0) {
+        char[] newCharacters = new char[characters.length - 1];
+        int j = 0;
+        cursorIndex--;
+        System.out.println(cursorIndex);
+        for (int i = 0; i < characters.length; i++) {
+            if (i != cursorIndex) {
+                newCharacters[i] = characters[j];
+                j++;
+            }
         }
-        j++;
+        str = new String(newCharacters);
+        charactersSinceNewLine--;
+        if (charactersSinceNewLine < 0) removeLine();
     }
-    cursorIndex--;
-    str = new String(newCharacters);
-    pixelHeight = (str.length() / charactersPerLine) * textSize;
   }
 
   protected void init(PApplet sketch){
@@ -151,17 +220,21 @@ public class InteractiveTextBox extends TextObject implements Serializable {
     //pixelWidth = sketch.textWidth(str);
   }
 
-  protected void handleCharacter(char key, int keyCode) {
+  protected void handleKey(char key, int keyCode) {
     super.handleKey(key, keyCode);
     if (key == sketch.CODED) {
         switch(keyCode) {
             case PConstants.UP:
+                moveCursor(Direction.UP);
                 break;
             case PConstants.DOWN:
+                moveCursor(Direction.DOWN);
                 break;
             case PConstants.LEFT:
+                moveCursor(Direction.LEFT);
                 break;
             case PConstants.RIGHT:
+                moveCursor(Direction.RIGHT);
                 break;
         }
     }
@@ -178,14 +251,16 @@ public class InteractiveTextBox extends TextObject implements Serializable {
                 break;
         }
     }
-    System.out.println(str);
   }
 
   // this function's text() is why this class does NOT extend TextBox
   protected void display(){
         super.display();
         sketch.textFont(pFont, textSize);
-        sketch.text(str, 0, 0, pixelWidth, pixelHeight);
+        sketch.textLeading(LINE_SPACING);
+        sketch.textAlign(PConstants.LEFT);
+        sketch.rectMode(PConstants.CORNER);
+        sketch.text(str, 0f - pixelWidth / 2, 0f - pixelHeight / 2, pixelWidth, pixelHeight + 1000);
         //sketch.text(str, 0, 0);
   }
 }
