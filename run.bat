62f76893624b7b9b9558e27a731fe34ec2256087
Mei Maddox <maddoxamei@gmail.com>
2020-02-23 17:27:42 -0500

    JavaDocs, Sources, Run bat

--- run.bat
+++ run.bat
@@ -2,6 +2,6 @@
 javac --release 8 -cp src/core.jar -d bin/ @sources
 
 cd bin
-java -cp ".;./core.jar" PollyPaint #This must be run from bin folder
+java -cp ".;./core.jar" LearningMyFriend #This must be run from bin folder
 cd ..
 PAUSE
