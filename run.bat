f3271c36f97afd577d9d498143b53d727cf842c2
Hunter Chasens <hunter.chasens18@ncf.edu>
2020-02-28 18:20:03 -0500

    new run.bat

--- run.bat
+++ run.bat
@@ -1,7 +1,6 @@
 @echo off
-javac -cp src/core.jar -d bin/ @sources
-
+javac -cp "./src/.;src/core.jar;src/javamp3-1.0.4.jar;src/jsyn-20171016.jar;src/sound.jar" -d bin/ @sources
 cd bin
-java -cp ".;./core.jar" LearningMyFriend #This must be run from bin folder
+java -cp ".;./core.jar;./javamp3-1.0.4.jar;./jsyn-20171016.jar;./sound.jar" LearningMyFriend #This must be run from bin folder
 cd ..
 PAUSE
