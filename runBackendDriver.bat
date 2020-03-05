4abcb25a583ed7c1f57607a75a08b5369f47eb1a
Isaac Bake <isaac@blake.earth>
2020-03-04 20:31:00 -0500

    Fix bat files.

--- runBackendDriver.bat
+++ runBackendDriver.bat
@@ -1,6 +1,6 @@
 @echo off
 javac -cp "./src/.;src/core.jar;src/javamp3-1.0.4.jar;src/jsyn-20171016.jar;src/sound.jar;src/gst1.jar;src/yt.jar;src/jna.jar;src/video.jar" -d bin/ @sources
 cd bin
-java -cp ".;./core.jar;./javamp3-1.0.4.jar;./jsyn-20171016.jar;./sound.jar;./gst1.jar;./yt.jar;./jna.jar;./video.jar" BackendDriver -Dgstreamer.library.path=src/windows32 #This must be run from bin folder
+java -cp ".;../lib/core.jar;../lib/javamp3-1.0.4.jar;../lib/jsyn-20171016.jar;../lib/sound.jar;../lib/gst1.jar;../lib/yt.jar;../lib/jna.jar;../lib/video.jar" BackendDriver -Dgstreamer.library.path=lib/windows32 #This must be run from bin folder
 cd ..
 PAUSE
